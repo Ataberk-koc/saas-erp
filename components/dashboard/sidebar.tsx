d24066a5824c9a52747d57e7f68a81257c138e98
Ataberk koç <ataberkoc123@outlook.com>
2026-02-09 20:01:55 +0300

    test

--- components/dashboard/sidebar.tsx
+++ components/dashboard/sidebar.tsx
@@ -69,6 +69,7 @@ export function Sidebar({ user, logoutBtn }: SidebarProps) {
             label="Ürünler"
             onClick={() => setIsOpen(false)}
           />
+          
 
           <SidebarLink
             href="/dashboard/invoices"
